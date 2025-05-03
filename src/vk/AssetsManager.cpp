d4b1cc813738ad82dc34f321b06ace656f6ae19b
ju-lit-hein <julien.ferdinand@epitech.eu>
2025-05-03 13:49:23 +0400

    style(vulkan_native_implementation): use keyword this when calling a class method

--- src/vk/AssetsManager.cpp
+++ src/vk/AssetsManager.cpp
@@ -9,13 +9,13 @@
 
 std::shared_ptr<maverik::FileAsset> maverik::vk::AssetsManager::addAsset(const std::string &path)
 {
-    if (assetExists(path)) {
+    if (this->assetExists(path)) {
         return std::make_shared<maverik::FileAsset>(_assets[path]);
     }
     std::uifstream file(path, std::ios::binary);
 
     if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << path << std::endl; // TODO use custom logger.
+        std::cerr << "Failed to open file: " << path << std::endl;
         return nullptr;
     }
     std::ustring content((std::istreambuf_iterator<unsigned char>(file)), std::istreambuf_iterator<unsigned char>());
