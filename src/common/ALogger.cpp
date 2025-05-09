79dcaf6d2a90c7ade974ca435cf48a71cbd0be90
ju-lit-hein <julien.ferdinand@epitech.eu>
2025-05-09 14:29:43 +0400

    fix(logger): remove forgotten old comment (#33)

--- src/common/ALogger.cpp
+++ src/common/ALogger.cpp
@@ -7,12 +7,8 @@
 
 #include "ALogger.hpp"
 
-// std::shared_ptr<maverik::ALogger> logger = nullptr;
-
 maverik::ALogger::~ALogger() = default;
 
-// TODO: all this code goes into the inhreited class
-// TODO: all this code goes into the log method
 void maverik::ALogger::fatal(const std::string &message, const std::string& caller) const
 {
     this->log(message, "\033[39;41mFATAL\033[39;49m     ", caller);
